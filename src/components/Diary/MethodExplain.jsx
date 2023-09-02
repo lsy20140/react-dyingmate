bdf9fb8ef4de3dfc2f6fd209d078fdd2361571d2
lsy20140 <lsy20140@naver.com>
2023-09-02 17:56:14 +0900

    feat: MethodExplain 컴포넌트 구현

---
+++ src/components/Diary/MethodExplain.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function MethodExplain({explain}) {
+  return (
+    <ExplainBox>
+      <p>· 전달메세지</p>
+      <p>{explain}</p>
+    </ExplainBox>
+  )
+}
+
+
+const ExplainBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  align-items: flex-start;
+  text-align: left;
+  width: 28rem;
+  background-color: #F0EAE0;
+  padding: 3.75rem 1.8rem;
+  color: var(--font-gray-3);
+  font-size: 1.125rem;
+  border-radius: 1.5rem;
+
+  p:nth-child(1) {
+    color: var(--main-color);
+    font-size: 1rem;
+  }
+`
