a0139e46789128144df783a8bc7822acfbc141fd
lsy20140 <lsy20140@naver.com>
2023-09-02 14:12:38 +0900

    feat: MethodItem 컴포넌트 구현

---
+++ src/components/Diary/MethodItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import {BsCheckLg} from 'react-icons/bs'
+
+export default function MethodItem({isSelected, itemText, handleOnClick}) {
+
+  return (
+    <>
+      <ItemBox isSelected={isSelected} onClick={handleOnClick}>
+        <p>{itemText}</p>
+        {isSelected && <BsCheckLg fontSize='1.75rem'/>}
+      </ItemBox>
+    </>
+  )
+}
+
+const ItemBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 4.375rem;
+  border-radius: 1rem;
+  
+  font-size: 1.25rem;
+  font-weight: 700;
+  background: ${props => props.isSelected ? '#F0EAE0' : 'rgba(240, 234, 224, 0.40)'};
+  color: ${props => props.isSelected ? `var(--main-color)` : 'white'};
+  align-items: center;
+  padding: 0 1.5rem;
+  margin-bottom: 0.75rem;
+
+  &:hover {
+    background-color: #F0EAE0;
+    color: var(--main-color);
+  }
+`
