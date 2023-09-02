eef69feb59a873eb850f5ef956e7047681946186
lsy20140 <lsy20140@naver.com>
2023-09-02 17:21:12 +0900

    feat: ProgressBar 컴포넌트 구현

---
+++ src/components/Diary/ProgressBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import ProgressBarOneStep from './ProgressBarOneStep'
+
+export default function ProgressBar({curIdx}) {
+  const arrIdx = new Array(4).fill(0);
+
+  return (
+    <div>
+      {arrIdx.map((item, i) => (
+        <OneStepWrapper>
+          <ProgressBarOneStep curStep={(i+1 <= curIdx) ? true: false} text={(i+1 !== 4) ? `Step ${i+1}` : 'Finish'}/>
+          {i!==3 && <Line curStep={(i+1 < curIdx) ? true: false}/>}
+        </OneStepWrapper>
+      ))}
+    </div>
+  )
+}
+
+
+const OneStepWrapper= styled.div`
+  display: flex;
+  align-items: flex-start;
+`
+
+const Line = styled.div`
+  width: 3.75rem;
+  border-top: 2px solid ${props => props.curStep ? '#FFB748' : 'white' };
+  margin: 1rem 0.75rem 0 0.75rem;
+
+`
