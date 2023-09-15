5371008ae21a46b9c2652aa19932db34c3b945c5
lsy20140 <lsy20140@naver.com>
2023-09-16 01:09:34 +0900

    test: Will 조회 api

--- src/components/PlayerRoom/MainModalContent/Will.jsx
+++ src/components/PlayerRoom/MainModalContent/Will.jsx
@@ -21,7 +21,7 @@ export default function Will() {
   const handleSubmit = async(e) => {
     // 유언장 추가 api 연동  
     axios.post(
-      'https://dying-mate-server.link/will/post',
+      '/will/post',
       {content: data},
       {withCredentials: true},
       
@@ -36,7 +36,7 @@ export default function Will() {
   }
 
   useEffect(() => {
-    axios.get('https://dying-mate-server.link/will/get?willId=5', {
+    axios.get('https://dying-mate-server.link/will/get?willId=2', {
 
       
     }, )
