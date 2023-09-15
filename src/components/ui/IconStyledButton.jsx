ee6727a5b484629fd0bb28e40e06ea454cbb45ac
lsy20140 <lsy20140@naver.com>
2023-09-15 12:09:48 +0900

    feat: IconStyledButton 컴포넌트 추가

---
+++ src/components/ui/IconStyledButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function IconStyledButton({width, text, fontWeight, color, btnColor, icon, handleOnClick}) {
+  const ButtonStyle = {
+    width: `${width}`,
+    color: `${color}`,
+    fontWeight: `${fontWeight}`,
+    borderRadius: "0.75rem",
+    backgroundColor: `${btnColor}`,
+    padding: "1rem 0",
+    border: "none",
+    display: 'flex',
+    gap: '0.75rem',
+    justifyContent: 'center',
+
+  }
+
+  return (
+    <button 
+      style={ButtonStyle}
+      onClick={(e) => {
+        e.preventDefault()
+        handleOnClick && handleOnClick()
+      }}
+    >
+      <p>{text}</p>
+      {icon}
+    </button>
+  )
+}
