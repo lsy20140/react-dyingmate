86e664a2416acac967bf056adfbdc8fae5e0882a
lsy20140 <lsy20140@naver.com>
2023-09-22 01:27:06 +0900

    feat: axios instane 생성

---
+++ src/apis/utils/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+// get, delete 등 인증이 필요하지 않은 경우
+const axiosApi = () => {
+  const instance = axios.create({baseURL: '/api/', withCredentials: true})
+  return instance;
+}
+
+// post, patch 등 인증이 필요한 경우
+const axiosAuthApi = () => {
+  const token = localStorage.getItem('login-token')
+
+  const instance =  axios.create({
+    baseURL: '/api/',
+    headers: {Authorization: 'Bearer' + token},
+    withCredentials: true
+  })
+  return instance
+}
+
+export const defaultInstance = axiosApi();
+export const authInstance = axiosAuthApi();
