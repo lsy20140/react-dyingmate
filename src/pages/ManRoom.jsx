456c3ec928cc0b2d5c73356e9fd1b7132dbbf390
lsy20140 <lsy20140@naver.com>
2023-10-06 03:34:29 +0900

    design: ManRoom 공간 에셋 추가

--- src/pages/ManRoom.jsx
+++ src/pages/ManRoom.jsx
@@ -1,48 +1,45 @@
-import React, {useRef, useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import { Canvas } from "@react-three/fiber";
 import { Man_Room } from '../components/models/ManRoom/ManRoom';
 import {OrbitControls} from '@react-three/drei'
-import CharInitDialog from '../components/ui/CharInitDialog';
 import { CameraControls } from '../Camera';
 import { useRoomFocus } from '../contexts/RoomFocus';
+import { Man } from '../components/models/ManRoom/Man';
+import CharMainDialog from '../components/ui/CharMainDialog';
+import { MessageArr } from '../data/man_script';
 
 export default function ManRoom() {
-  const dialogRef = useRef();
-  const roomRef = useRef();
-  // const {focus} = useRoomFocus();
-
-  const [position, setPosition] = useState({ x: 28, y: 8, z: 0 });
+  const {focus} = useRoomFocus();
+  const [position, setPosition] = useState({ x: 24, y: 8, z: 0 });
   const [target, setTarget] = useState({ x: 0, y: 0, z: 0 });
 
-
-  // useEffect(() => {
-  //   console.log("GrandmaRoomfocus", focus)
-  //   console.log("dialogRef",dialogRef);
-  //   if(focus) {
-  //     console.log("focus!!!")
-  //     setPosition({ x: 0, y: 3, z: 1 });
-  //     setTarget({ x: -4, y: 0, z: 0 });
-  //   }
-  // },[focus])
-
-
+  useEffect(() => {
+    if(focus) {
+      setPosition({ x: 0, y: 3, z: 1 });
+      setTarget({ x: -4, y: 0, z: 0 });
+    }
+    else {
+      setPosition({ x: 24, y: 8, z: 0 });
+      setTarget({ x: 0, y: 0, z: 0});
+    }
+  },[focus])
 
   return (
     <>
-      <Canvas camera={{fov: 30, position:[28,8,0]}} colorManagement>
+      <Canvas camera={{fov: 30, position:[24,8,0]}} colorManagement>
         <OrbitControls/>
         {/* <LightHelper /> */}
         <axesHelper args={[200, 200, 200]} />
-        <ambientLight intensity={0.1} />
-        <directionalLight intensity={1}  decay={2} color="#eca864" position={[ 17, 12.421, -2]} target-position={[0, 9, 2]} />
-        <directionalLight intensity={1.2} castShadow decay={2} color="#d8b58d" position={[22, 15.344, -5]} target-position={[2, 10, 0]} />
+        <ambientLight intensity={50} />
+        <directionalLight intensity={8}  decay={1} color="#ffffff" position={[ 20, 3, -2]} target-position={[-5, 2, 2]} />
+        <directionalLight intensity={10} castShadow decay={2} color="#ffffff" position={[20, 5, -5]} target-position={[2, 2, 0]} />
         <CameraControls position={position} target={target} />
-        <group ref={roomRef} rotation-y={-Math.PI} rotation-z={-Math.PI/10} position-y={-5}>
+        <group rotation-y={-Math.PI} rotation-z={-Math.PI/10} position-y={-5}>
           <Man_Room/>
+          <Man/>
         </group>
       </Canvas>
-      {/* {!focus && <CharInitDialog />} */}
-      
+      <CharMainDialog messageArr={MessageArr} />
     </>
   )
 }
